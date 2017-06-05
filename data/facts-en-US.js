59a6252addbb76cb25546bf90a02d779c950ef88
StevenSawtelle <steven.sawtelle@gmail.com>
2017-06-04 20:46:22 -0700

    init commit, awaiting ceritification

---
+++ data/facts-en-US.js
@@ -0,0 +1,48 @@
+var data = [
+    "There are over 300 palm trees on the ASU campus.",
+    "There are over 40 varieties of palm trees on the ASU campus.",
+    "The iconic Palm Walk turned 100 years old in 2016.",
+    "The original idea for Palm Walk came from ASU President Arthur John Matthews.",
+    "In 1987, Pope John Paul II held a mass at Sun Devil Stadium.",
+    "When Pope John Paul II visited ASU, he requested the university cover any mention of Sparky or pitchforks while he was there.",
+    "ASU has changed its name five times.",
+    "The former names of ASU are Arizona State College at Tempe, the Tempe State Teachers College, the Tempe Normal School of Arizona, the Normal School of Arizona and the Territorial Normal School.",
+    "Arizona State University has been the name of the university for 59 years",
+    "Faculty and students have reported several ghost sightings at the Matthews Center, the Community Services Building, the Virginia G. Piper Writers House and other buildings to ASU officials",
+    "Ghost sightings at ASU have become so common that officials have compiled a list of the most frequent locations.",
+    "Territorial Normal School, ASU's original name, was founded in 1885.",
+    "The first class of students at ASU was just thirty-three people large.",
+    "Arizona State University and University of Arizona compete anually for the oldest trophy in NCAA history in a game called the Territorial Cup.",
+    "The Terriorial Cup, in which Arizona State University and University of Arizona compete anually, has been held since 1899.",
+    "Sun Devil Stadium is the only football stadium located between two mountains.",
+    "In its early years, it was common to see cows wandering around the ASU campus, as it was built on a former cow pasture",
+    "The original mascot of ASU under the Tempe Normal School name was the normals.",
+    "In 1899, Tempe Normal School changed its mascot from the normals to the owls.",
+    "In 1922, the mascot changed from the owls to the bulldogs.",
+    "Walt Disney was the original artist to draw the mascot Sparky.",
+    "Sparky was adapted as the mascot for ASU in 1951.",
+    "ASU was rated the number one university in the United States for innovation in the 2016 and 2017 US News and World Report.",
+    "In the Fall of 2016, there was a total of 98,146 students registered across all campuses.",
+    "The largest growing campus recently is the Skysong Campus, from six thousand in Fall 2012 to twenty-sex thousand in Fall 2016",
+    "Over 300 majors are offered at ASU.",
+    "ASU was founded before Arizona was even an official state in the United States.",
+    "ASU is home to over 1,100 clubs.",
+    "Students from over 150 countris attend ASU.",
+    "The average of GPA of high school students accepted to ASU is 3.42.",
+    "There are 493 total classrooms across ASU's campuses.",
+    "Over ninety percent of undergraduate students at ASU work on individual projects like capstones or thesis projects with faculty members.",
+    "Over 100 baseball players from ASU have gone on to play in the MLB.",
+    "A mountain, the iconic location near the Tempe Campus, has the official name of Tempe Butte.",
+    "The iconic A mountatin originally had a T and N on it instead, for Tempe Normal School.",
+    "ASU directly employs over twenty thousand people and dispensed nearly one billion dollars in wages anually.",
+    "Though the name has changed multiple times, the official school colors of ASU have been Maroon and Gold since 1896.",
+    "Notable comedians who have attended ASU include Jimmy Kimmel, David Spade, and Steve Allen.",
+    "The Sun Devil Marching Band is extremely successful, being one of only twenty eight to win the prestigious Sudler Trophy.",
+    "100 million dollars in venture capital funds have been invested in companies mentored by ASU Technopolis."
+];
+
+var SKILL_NAME = "A.S.U. Facts";
+var GET_FACT_MESSAGE = "Here's your fact: ";
+var HELP_MESSAGE = "You can say tell me an A.S.U. fact, or, you can say exit... What can I help you with?";
+var HELP_REPROMPT = "What can I help you with?";
+var STOP_MESSAGE = "Goodbye!";
